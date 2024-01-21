f02bedf7a993adf8370fd5863fedfdf126517bcc
zubair8934 <zubair6986597@gmail.com>
2024-01-22 02:23:16 +0500

    Removed static folder and updated views.py to access images from s3 bucket

--- flask/app/views.py
+++ flask/app/views.py
@@ -18,7 +18,7 @@ def cat():
 	table.update_item(Key={'server_id': 1},
         UpdateExpression=f"SET {'cat_server_count'} = {'cat_server_count'} + :val", 
         ExpressionAttributeValues={':val': 1})
-	cat_image_url = "static/cat.jpg"
+	cat_image_url = get_s3_url('cat.jpg')
 	return render_template('cat.html', cat_image_url=cat_image_url)
 
 @app.route('/dog')
@@ -28,7 +28,7 @@ def dog():
 	table.update_item(Key={'server_id': 1},
         UpdateExpression=f"SET {'dog_server_count'} = {'dog_server_count'} + :val",
         ExpressionAttributeValues={':val': 1})
-	dog_image_url = "static/dog.jpg"
+	dog_image_url = get_s3_url('dog.jpg')
 	return render_template('dog.html', dog_image_url=dog_image_url)
 
 @app.route('/count')
@@ -42,6 +42,11 @@ def count():
         return render_template('request_count.html', cat_server_count=cat_server_count, dog_server_count=dog_server_count)
     else:
         return "Item not found"
+	
+def get_s3_url(object_key):
+    s3 = boto3.client('s3', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    url = s3.generate_presigned_url('get_object', Params={'Bucket': "flask-s3-8934", 'Key': object_key}, ExpiresIn=3600)
+    return url
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5000)

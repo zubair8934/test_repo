a0055964c2e7110d018cb7af778c7b9f6da947c1
zubair8934 <zubair6986597@gmail.com>
2024-01-23 14:39:18 +0500

    removed access key and secret key

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,11 +1,12 @@
 from app import app
 from flask import render_template
 import boto3
-import os
 
-aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-aws_region = os.environ.get('AWS_REGION', 'us-east-1')
+def get_s3_url(object_key):
+    s3 = boto3.client('s3', region_name='us-east-1')
+    
+    url = s3.generate_presigned_url('get_object', Params={'Bucket': "flask-s3-8934", 'Key': object_key}, ExpiresIn=3600)
+    return url
 
 @app.route('/')
 def index():
@@ -13,27 +14,32 @@ def index():
 
 @app.route('/cat')
 def cat():
-	dynamodb = boto3.resource('dynamodb', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-	table = dynamodb.Table('ServerCount')
-	table.update_item(Key={'server_id': 1},
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    
+    table = dynamodb.Table('ServerCount')
+    table.update_item(Key={'server_id': 1},
         UpdateExpression=f"SET {'cat_server_count'} = {'cat_server_count'} + :val", 
         ExpressionAttributeValues={':val': 1})
-	cat_image_url = get_s3_url('cat.jpg')
-	return render_template('cat.html', cat_image_url=cat_image_url)
+    
+    cat_image_url = get_s3_url('cat.jpg')
+    return render_template('cat.html', cat_image_url=cat_image_url)
 
 @app.route('/dog')
 def dog():
-	dynamodb = boto3.resource('dynamodb', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-	table = dynamodb.Table('ServerCount')
-	table.update_item(Key={'server_id': 1},
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    
+    table = dynamodb.Table('ServerCount')
+    table.update_item(Key={'server_id': 1},
         UpdateExpression=f"SET {'dog_server_count'} = {'dog_server_count'} + :val",
         ExpressionAttributeValues={':val': 1})
-	dog_image_url = get_s3_url('dog.jpg')
-	return render_template('dog.html', dog_image_url=dog_image_url)
+    
+    dog_image_url = get_s3_url('dog.jpg')
+    return render_template('dog.html', dog_image_url=dog_image_url)
 
 @app.route('/count')
 def count():
-    dynamodb = boto3.resource('dynamodb', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
     table = dynamodb.Table('ServerCount')
     response = table.get_item(Key={'server_id': 1})
     if 'Item' in response:
@@ -42,11 +48,6 @@ def count():
         return render_template('request_count.html', cat_server_count=cat_server_count, dog_server_count=dog_server_count)
     else:
         return "Item not found"
-	
-def get_s3_url(object_key):
-    s3 = boto3.client('s3', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    url = s3.generate_presigned_url('get_object', Params={'Bucket': "flask-s3-8934", 'Key': object_key}, ExpiresIn=3600)
-    return url
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5000)
